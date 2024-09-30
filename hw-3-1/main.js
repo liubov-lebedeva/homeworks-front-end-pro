68e42506e541d0c1c38bd02c9d77b8a2de9c3a94
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-09-30 15:34:17 +0300

    Completed the assignment

---
+++ hw-3-1/main.js
@@ -0,0 +1,12 @@
+let a;
+console.log(`Primitive values:
+${typeof 5}
+${typeof "a"}
+${typeof true}
+${typeof a}
+${typeof 1n}
+${typeof Symbol()}
+
+Objects:
+array (function, object, etc.) is a ${typeof []}
+also, typeof returns "${typeof null}" for null`);
