9d88fa731218c4c3810909a09cd10f1fafab453a
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-10-01 23:12:32 +0300

    Improved my code for better user experience

--- hw-4-1/main.js
+++ hw-4-1/main.js
@@ -1,2 +1,5 @@
-const usersName = prompt ("What's your name?");
-alert (`Hello, ${usersName}! How are you?`);
+let usersName = prompt ("What's your name?");
+usersName = usersName.trim();
+if (usersName) {
+    alert (`Hello, ${usersName}! How are you?`);
+} else alert ("I just wanted to know your name...");
