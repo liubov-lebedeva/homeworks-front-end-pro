a51f572dfdf46fb825bcd6e3f6cb0d13219a5fb8
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-10-11 18:56:00 +0300

    Complete the assignment

---
+++ hw-6-1/main.js
@@ -0,0 +1,18 @@
+const inputString = prompt("Input string:");
+const inputSymbols = prompt("Input comma-separated symbols:");
+const arrayOfSymbols = inputSymbols.split(",");
+
+
+function removeSymbols(line, symbols) {
+    let resultString = "";
+    for (let i = 0; i < line.length; i++) {
+        if (symbols.includes(line[i])) {
+            resultString += "";
+        } else {
+            resultString += line[i];
+        }
+    }
+    return resultString;
+}
+
+console.log(removeSymbols(inputString, arrayOfSymbols));
