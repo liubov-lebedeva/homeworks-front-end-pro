084839a5db54b37a675e1d07d29a81cd2ea8b263
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-10-13 20:52:07 +0300

    Improve code using closures, make code reusable. Include (sum) inside the function using closures, so the state (sum) is private to that function instance, making the code reusable. Now I can reuse this function with another variable

--- hw-7-1/main.js
+++ hw-7-1/main.js
@@ -1,11 +1,22 @@
-let sum = 0;
-
-let sumOfNumbers = (num) => {
-    sum += num;
-    return sum;
+const getSumOfNumbersFunction = () => {
+    let sum = 0;
+    return (num) => {
+        sum += num;
+        return sum;
+    }
 }
+// Include (sum) inside the function using closures, so the state (sum) is private to that function instance, making the code reusable
+const sumOfNumbers = getSumOfNumbersFunction();
 
 console.log(sumOfNumbers(4));
 console.log(sumOfNumbers(6));
 console.log(sumOfNumbers(10));
 console.log(sumOfNumbers(7));
+
+// Now I can reuse this function with another variable
+const anotherSumOfNumbers = getSumOfNumbersFunction();
+
+console.log(anotherSumOfNumbers(10));
+console.log(anotherSumOfNumbers(4));
+console.log(anotherSumOfNumbers(10));
+console.log(anotherSumOfNumbers(2));
