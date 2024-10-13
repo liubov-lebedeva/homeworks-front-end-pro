e0fc654050c949bf543091144a8e83ecc0ec6dd2
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-10-13 20:39:53 +0300

    Complete the assignment

---
+++ hw-7-1/main.js
@@ -0,0 +1,11 @@
+let sum = 0;
+
+let sumOfNumbers = (num) => {
+    sum += num;
+    return sum;
+}
+
+console.log(sumOfNumbers(4));
+console.log(sumOfNumbers(6));
+console.log(sumOfNumbers(10));
+console.log(sumOfNumbers(7));
