b47b5395f20d613dd2128684eec4bbed901de439
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-10-14 21:45:30 +0300

    Complete the assignment

---
+++ hw-8-1/main.js
@@ -0,0 +1,17 @@
+let ladder = {
+    step: 0,
+    up: function () {
+        this.step++;
+        return this;
+    },
+    down: function () {
+        this.step--;
+        return this;
+    },
+    showStep: function () {
+        console.log(this.step);
+        return this;
+    }
+};
+
+ladder.up().up().down().showStep();
