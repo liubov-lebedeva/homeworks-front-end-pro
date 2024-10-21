779cbb6bda5aa30e97afcabce125b2e55cc3169f
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-10-21 14:47:58 +0300

    Setup test data

---
+++ hw-9-1/main.js
@@ -0,0 +1,12 @@
+const company = {
+    sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
+    development: {
+        web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
+        internals: [{name: 'Jack', salary: 1300}],
+        QA: {
+            manual: [{name: 'Dima', salary: 3500}, {name: 'Masha', salary: 1000}],
+            automation: [{name: 'Pavlo', salary: 500}, {name: 'Alex', salary: 500}, {name: 'Mark', salary: 1000}],
+        }
+    },
+    SMM: [{name: 'Denis', salary: 400}, {name: 'Ira', salary: 600}]
+};
