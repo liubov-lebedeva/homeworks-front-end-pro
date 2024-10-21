42eb02b669f5e7edf4d1288210ccdcb1893ecb83
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-10-21 16:07:52 +0300

    Complete the assignment

--- hw-9-1/main.js
+++ hw-9-1/main.js
@@ -1,20 +1,30 @@
 const company = {
     sales: [{name: 'John', salary: 1000}, {name: 'Alice', salary: 600}],
     development: {
-        web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 1800}],
-        internals: [{name: 'Jack', salary: 1300}],
+        web: [{name: 'Peter', salary: 2000}, {name: 'Alex', salary: 2000}],
+        internals: [{name: 'Jack', salary: 1000}],
         QA: {
-            manual: [{name: 'Dima', salary: 3500}, {name: 'Masha', salary: 1000}],
+            manual: [{name: 'Dima', salary: 3400}, {name: 'Masha', salary: 1000}],
             automation: [{name: 'Pavlo', salary: 500}, {name: 'Alex', salary: 500}, {name: 'Mark', salary: 1000}],
         }
     },
-
+    SMM: [{name: 'Denis', salary: 400}, {name: 'Ira', salary: 600}]
 };
 
-function sumOfSalariesInArray (array) {
-    return array.reduce((sum, employee) => sum + employee.salary, 0);
+function getSalariesSum(obj) {
+    let sumSalaries = 0;
+
+    for (let key in obj) {
+        if (Array.isArray(obj[key])) {
+            sumSalaries += obj[key].reduce((sum, employee) => sum + employee.salary, 0);
+        } else if (typeof obj[key] === 'object') {
+            sumSalaries += getSalariesSum(obj[key]);
+        }
+    }
+    return sumSalaries;
 }
 
-const SMM = [{name: 'Denis', salary: 400}, {name: 'Ira', salary: 600}];
+console.log(getSalariesSum(company));
 
-console.log(sumOfSalariesInArray(SMM));
+
+
