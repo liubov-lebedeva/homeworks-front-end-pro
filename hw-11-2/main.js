8891ccab1f37eabcf428192a4fe9ea726d322e9f
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-10-28 22:33:29 +0200

    Replaced solution with toggle.

--- hw-11-2/main.js
+++ hw-11-2/main.js
@@ -1,9 +1,5 @@
 const textBlock = document.getElementById('textBlock');
 const changeColorButton = document.getElementById('changeColorButton');
 
-let isOriginalColor = true;
 
-changeColorButton.addEventListener('click', () => {
-    textBlock.style.color = isOriginalColor ? 'red' : 'black';
-    isOriginalColor = !isOriginalColor;
-});
+changeColorButton.addEventListener('click', () => textBlock.classList.toggle('textBlockToggled'));
