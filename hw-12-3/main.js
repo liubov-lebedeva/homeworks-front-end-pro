f36c24b8f44595d0a3f4a6266a6f95b9d6e6fd89
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-08 08:27:42 +0200

    Transfer getElementById to separate valuable

--- hw-12-3/main.js
+++ hw-12-3/main.js
@@ -1,3 +1,5 @@
+const taskList = document.getElementById('task-list');
+
 document.getElementById("add-task-button").addEventListener("click", function () {
     const taskInput = document.getElementById("task-input");
     const taskText = taskInput.value.trim();
@@ -5,16 +7,16 @@ document.getElementById("add-task-button").addEventListener("click", function ()
     if (taskText) {
         const newTask = document.createElement("li");
         newTask.innerHTML = `${taskText} <button class="remove-btn">Remove</button>`;
-        document.getElementById('task-list').appendChild(newTask);
+        taskList.appendChild(newTask);
         taskInput.value = '';
     } else {
         alert("Please enter a task.");
     }
 });
 
-document.getElementById('task-list').addEventListener('click', function (event) {
+taskList.addEventListener('click', function (event) {
     if (event.target.classList.contains('remove-btn')) {
         const taskItem = event.target.parentElement;
-        document.getElementById('task-list').removeChild(taskItem);
+        taskList.removeChild(taskItem);
     }
 });
