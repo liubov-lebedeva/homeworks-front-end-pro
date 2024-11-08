dd4565de5fb8d41b4970a73a70f579c48167e045
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-08 08:37:20 +0200

    Formatting

--- hw-12-2/main.js
+++ hw-12-2/main.js
@@ -1,7 +1,7 @@
 const buttonContainer = document.getElementById('button-container');
 
 
-buttonContainer.addEventListener('click', function(event) {
+buttonContainer.addEventListener('click', function (event) {
     if (event.target.tagName === 'BUTTON') {
         alert(`Button pressed: ${event.target.textContent}`);
     }
