18f4aba623cc264977f9f15f195f480e751393a5
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-16 16:53:37 +0200

    Optimized few areas: filling new array maxAttendancesAmount, replace while by reduce,add warning about full attendance, add Student without attendances.

--- hw-16-1/main.js
+++ hw-16-1/main.js
@@ -5,7 +5,7 @@ class Student {
         this.birthYear = birthYear;
         this.grades = grades;
         this.maxAttendancesAmount = 25;
-        this.attendances = new Array(this.maxAttendancesAmount);
+        this.attendances = new Array(this.maxAttendancesAmount).fill(null);
         this.attendancesAmount = 0;
         this.minGradePointAverage = 90;
         this.minAverageAttendance = 0.9;
@@ -19,19 +19,17 @@ class Student {
         if (this.grades.length === 0) {
             return 0;
         }
-        let gradesSum = 0;
-        let gradeIndex = 0;
-        while (gradeIndex <= this.grades.length - 1) {
-            gradesSum += this.grades[gradeIndex];
-            gradeIndex++;
-        }
+        const gradesSum = this.grades.reduce((sum, grade) => sum + grade, 0);
         return gradesSum / this.grades.length;
     }
+
 
     present() {
         if (this.attendancesAmount < this.maxAttendancesAmount) {
             this.attendances[this.attendancesAmount] = true;
             this.attendancesAmount++;
+        } else {
+            console.warn("Attendance is full. Cannot mark more than 25 records.");
         }
     }
 
@@ -39,18 +37,19 @@ class Student {
         if (this.attendancesAmount < this.maxAttendancesAmount) {
             this.attendances[this.attendancesAmount] = false;
             this.attendancesAmount++;
+        } else {
+            console.warn("Attendance is full. Cannot mark more than 25 records.");
         }
     }
 
     getAverageAttendance() {
-        let visitsAmount = 0;
-        for (let visitIndex = 0; visitIndex < this.attendancesAmount; visitIndex++) {
-            if (this.attendances[visitIndex]) {
-                visitsAmount++;
-            }
+        if (this.attendancesAmount === 0) {
+            return 0;
         }
+        const visitsAmount = this.attendances.slice(0, this.attendancesAmount).filter(Boolean).length;
         return visitsAmount / this.attendancesAmount;
     }
+
 
     summary() {
         let gradePointAverage = this.getGradePointAverage();
@@ -73,20 +72,52 @@ john.present();
 john.absent();
 
 console.log(john);
-
-console.log(john.getAge());
-console.log(john.getGradePointAverage());
-console.log(john.getAverageAttendance());
-console.log(john.summary());
+console.log(`Age: ${john.getAge()}`);
+console.log(`Grade Point Average: ${john.getGradePointAverage()}`);
+console.log(`Average Attendance: ${john.getAverageAttendance()}`);
+console.log(`Summary: ${john.summary()}`);
 
 
 const betty = new Student('Betty', 'Treal', 2000, [90, 100, 70, 80, 100, 100]);
 betty.absent();
 betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.absent();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
+betty.present();
 
 console.log(betty);
+console.log(`Age: ${betty.getAge()}`);
+console.log(`Grade Point Average: ${betty.getGradePointAverage()}`);
+console.log(`Average Attendance: ${betty.getAverageAttendance()}`);
+console.log(`Summary: ${betty.summary()}`);
 
-console.log(betty.getAge());
-console.log(betty.getGradePointAverage());
-console.log(betty.getAverageAttendance());
-console.log(betty.summary());
+
+
+const mark = new Student('Mark', 'Smith', 2001, []);
+console.log(mark);
+console.log(`Age: ${mark.getAge()}`);
+console.log(`Grade Point Average: ${mark.getGradePointAverage()}`);
+console.log(`Average Attendance: ${mark.getAverageAttendance()}`);
+console.log(`Summary: ${mark.summary()}`);
