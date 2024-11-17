860fbebb92ed7bcbc2504a7fcdaf3180809588f4
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-17 23:46:53 +0200

    Used constructor function instead of class; remove this.attendancesAmount; replaced this.attendancesAmount with this.attendances.length; used push to add true or false to attendances array; removed pre-filling arrays without initial values. In the future I can move reusable methods on the Prototype: this approach is more memory-efficient because all instances of Student will share the same method definitions, instead of each instance having its own copies

--- hw-16-1/main.js
+++ hw-16-1/main.js
@@ -1,59 +1,54 @@
-class Student {
-    constructor(firstName, lastName, birthYear, grades) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.birthYear = birthYear;
-        this.grades = grades;
-        this.maxAttendancesAmount = 25;
-        this.attendances = new Array(this.maxAttendancesAmount).fill(null);
-        this.attendancesAmount = 0;
-        this.minGradePointAverage = 90;
-        this.minAverageAttendance = 0.9;
-    }
+function Student(firstName, lastName, birthYear, grades) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthYear = birthYear;
+    this.grades = grades;
+    this.maxAttendancesAmount = 25;
+    this.attendances = [];
+    this.minGradePointAverage = 90;
+    this.minAverageAttendance = 0.9;
 
-    getAge() {
+    this.getAge = function () {
         return new Date().getFullYear() - this.birthYear;
-    }
+    };
 
-    getGradePointAverage() {
+    this.getGradePointAverage = function () {
         if (this.grades.length === 0) {
             return 0;
         }
         const gradesSum = this.grades.reduce((sum, grade) => sum + grade, 0);
         return gradesSum / this.grades.length;
-    }
+    };
 
 
-    present() {
-        if (this.attendancesAmount < this.maxAttendancesAmount) {
-            this.attendances[this.attendancesAmount] = true;
-            this.attendancesAmount++;
+    this.present = function () {
+        if (this.attendances.length < this.maxAttendancesAmount) {
+            this.attendances.push(true);
         } else {
             console.warn("Attendance is full. Cannot mark more than 25 records.");
         }
-    }
+    };
 
-    absent() {
-        if (this.attendancesAmount < this.maxAttendancesAmount) {
-            this.attendances[this.attendancesAmount] = false;
-            this.attendancesAmount++;
+    this.absent = function () {
+        if (this.attendances.length < this.maxAttendancesAmount) {
+            this.attendances.push(false);
         } else {
             console.warn("Attendance is full. Cannot mark more than 25 records.");
         }
-    }
+    };
 
-    getAverageAttendance() {
-        if (this.attendancesAmount === 0) {
+    this.getAverageAttendance = function () {
+        if (this.attendances.length === 0) {
             return 0;
         }
-        const visitsAmount = this.attendances.slice(0, this.attendancesAmount).filter(Boolean).length;
-        return visitsAmount / this.attendancesAmount;
-    }
+        const visitsAmount = this.attendances.filter(Boolean).length;
+        return visitsAmount / this.attendances.length;
+    };
 
 
-    summary() {
-        let gradePointAverage = this.getGradePointAverage();
-        let averageAttendance = this.getAverageAttendance();
+    this.summary = function () {
+        const gradePointAverage = this.getGradePointAverage();
+        const averageAttendance = this.getAverageAttendance();
         if (gradePointAverage >= this.minGradePointAverage && averageAttendance >= this.minAverageAttendance) {
             return 'Well done!';
         } else if (gradePointAverage >= this.minGradePointAverage || averageAttendance >= this.minAverageAttendance) {
@@ -61,7 +56,7 @@ class Student {
         } else {
             return 'Rediska';
         }
-    }
+    };
 }
 
 const john = new Student('John', 'Bahh', 1999, [90, 80, 70, 100, 100, 100]);
@@ -78,41 +73,21 @@ console.log(`Average Attendance: ${john.getAverageAttendance()}`);
 console.log(`Summary: ${john.summary()}`);
 
 
-const betty = new Student('Betty', 'Treal', 2000, [90, 100, 70, 80, 100, 100]);
-betty.absent();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.absent();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
-betty.present();
+const betty = new Student("Betty", "Treal", 1990, [90, 100, 70, 80, 100, 100]);
+for (let i = 0; i < 25; i++) {
+    if (i < 24) {
+        betty.present();
+    } else {
+        betty.absent();
+    }
+}
+
 
 console.log(betty);
 console.log(`Age: ${betty.getAge()}`);
 console.log(`Grade Point Average: ${betty.getGradePointAverage()}`);
 console.log(`Average Attendance: ${betty.getAverageAttendance()}`);
 console.log(`Summary: ${betty.summary()}`);
-
 
 
 const mark = new Student('Mark', 'Smith', 2001, []);
