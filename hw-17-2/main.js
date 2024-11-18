b4aeac6c43355910a2f3f4fce0c9a6f11fb77c26
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-18 18:35:51 +0200

    Completed the assignment

--- hw-17-2/main.js
+++ hw-17-2/main.js
@@ -10,3 +10,10 @@ class Coach {
         console.log(`Coach: ${this.name}, Specialization: ${this.specialization}, Rating: ${this.rating}`)
     }
 }
+
+
+const coach1 = new Coach('John Doe', 'Fitness', 4.7);
+const coach2 = new Coach('Alice Smith', 'Yoga', 4.9);
+
+coach1.displayInfo();
+coach2.displayInfo();
