f57bc2db8d97388591ff1fc6bedfba73b0a15fa1
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-18 18:52:17 +0200

    Completed the assignment

--- hw-17-3/main.js
+++ hw-17-3/main.js
@@ -18,3 +18,12 @@ class bankAccount {
         amount > this.balance ? console.warn("Insufficient funds.") : (this.balance -= amount);
     }
 }
+
+
+const account1 = new bankAccount(1000);
+
+console.log(account1.getBalance());
+account1.deposit(500);
+console.log(account1.getBalance());
+account1.withdraw(200);
+console.log(account1.getBalance());
