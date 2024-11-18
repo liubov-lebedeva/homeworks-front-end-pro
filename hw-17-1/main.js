ec2b9281ba93f8d08a9ccd37d76598bf44ed9032
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-18 18:27:30 +0200

    Completed the assignment

--- hw-17-1/main.js
+++ hw-17-1/main.js
@@ -20,3 +20,9 @@ class Calculator {
         return b === 0 ? "Cannot divide by zero" : a / b;
     }
 }
+
+const calc = new Calculator();
+console.log(calc.add(5, 3));
+console.log(calc.subtract(10, 4));
+console.log(calc.multiply(3, 6));
+console.log(calc.divide(8, 2));
