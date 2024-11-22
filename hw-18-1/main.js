bf2f11c1fa8c57f086b9d5737abe0188e007025d
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-22 18:42:54 +0200

    Added HTML element "timer". Initialized initial time, get timer element from HTML, created function to convert seconds to minutes

---
+++ hw-18-1/main.js
@@ -0,0 +1,10 @@
+let initialTime = 87;
+
+const timer = document.getElementById("timer");
+
+function convertTimeFormat(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
+}
+
