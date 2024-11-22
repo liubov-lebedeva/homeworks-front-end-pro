38d43f99e90b5cf78162e6a1899ce094b5926695
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-22 19:09:20 +0200

    Created function that starts timer, added there setInterval to count each second. When all seconds are up, there appears message "time is up". Also added styles for timer and message

--- hw-18-1/main.js
+++ hw-18-1/main.js
@@ -8,3 +8,20 @@ function convertTimeFormat(seconds) {
     return `${minutes.toString().padStart(2, "0")}:${remainingSeconds.toString().padStart(2, "0")}`;
 }
 
+
+function startTimer(duration) {
+    let remainingTime = duration;
+
+    const interval = setInterval(() => {
+        timer.textContent = convertTimeFormat(remainingTime);
+        if (remainingTime === 0) {
+            clearInterval(interval);
+            timer.textContent = "Time is up";
+            timer.classList.add("time-up");
+        } else {
+            remainingTime--;
+        }
+    }, 1000);
+}
+
+startTimer(initialTime);
