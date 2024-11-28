cbdaca5bbd489b79debb506dbb9724f54a84e5b7
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-11-28 17:26:29 +0200

    Added information about humidity, pressure and wind. Changes styles

--- hw-19-1/main.js
+++ hw-19-1/main.js
@@ -20,13 +20,19 @@ async function fetchWeather() {
 
 
 function displayWeather(data) {
-    const {main, weather, name} = data;
+    const {main, weather, name, wind} = data;
     const tempCelsius = (main.temp - 273.15).toFixed(2);
     const feelsLikeCelsius = (main.feels_like - 273.15).toFixed(2);
+    const humidity = main.humidity;
+    const pressure = main.pressure;
+    const windSpeed = wind.speed;
+    const windDeg = wind.deg;
+
+    const windDirection = getWindDirection(windDeg);
 
     const currentTime = new Date();
-    const dateOptions = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-    const timeOptions = { hour: '2-digit', minute: '2-digit', hour12: true };
+    const dateOptions = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    const timeOptions = {hour: '2-digit', minute: '2-digit', hour12: true};
     const formattedDate = currentTime.toLocaleDateString('en-US', dateOptions);
     const formattedTime = currentTime.toLocaleTimeString('en-US', timeOptions);
 
@@ -36,11 +42,27 @@ function displayWeather(data) {
             <p>${weather[0].description}</p>
             <p>Temperature: ${tempCelsius}°C</p>
             <p>Feels like: ${feelsLikeCelsius}°C</p>
+            <p>Humidity: ${humidity}%</p>
+            <p>Pressure: ${pressure} hPa</p>
+            <p>Wind: ${windSpeed} m/s (${windDirection})</p>
             <p class="time">${formattedDate}</p>
             <p class="time">${formattedTime}</p>
         </div>
     `;
 }
+
+
+    function getWindDirection(deg) {
+        if (deg >= 0 && deg < 22.5) return 'N';
+        if (deg >= 22.5 && deg < 67.5) return 'NE';
+        if (deg >= 67.5 && deg < 112.5) return 'E';
+        if (deg >= 112.5 && deg < 157.5) return 'SE';
+        if (deg >= 157.5 && deg < 202.5) return 'S';
+        if (deg >= 202.5 && deg < 247.5) return 'SW';
+        if (deg >= 247.5 && deg < 292.5) return 'W';
+        if (deg >= 292.5 && deg < 337.5) return 'NW';
+        return 'N';
+    }
 
 
 fetchWeather();
