34f039639b9d86fe380fa3053a104932842fbb1b
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-12-05 17:45:36 +0200

    Optimized JS-code, using jQuery. Added styles for modal, to wrap content in the window

--- hw-20-1/main.js
+++ hw-20-1/main.js
@@ -1,91 +1,72 @@
-const form = document.querySelector(".js--form");
-const input = document.querySelector(".js--form__input");
-const todosWrapper = document.querySelector(".js--todos-wrapper");
+$(document).ready(function () {
+    let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
+    let serialNumber = +localStorage.getItem("serialNumber") || 0;
 
-let savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
-let serialNumber = +localStorage.getItem("serialNumber") || 0;
-
-function initializeTodos() {
-    savedTodos.forEach(todo => {
-        addTodoToDOM(todo.id, todo.text, todo.completed);
-    });
-}
-
-form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const taskText = input.value.trim();
-    if (taskText) {
-        const id = createId();
-        addTodoToDOM(id, taskText, false);
-        saveToLocalStorage(id, taskText, false);
-        input.value = "";
+    function initializeTodos() {
+        savedTodos.forEach(todo => {
+            addTodoToDOM(todo.id, todo.text, todo.completed);
+        });
     }
+
+
+    $(".js--form").on("submit", function (e) {
+        e.preventDefault();
+        const taskText = $(".js--form__input").val().trim();
+        if (taskText) {
+            const id = createId();
+            addTodoToDOM(id, taskText, false);
+            saveToLocalStorage(id, taskText, false);
+            $(".js--form__input").val("");
+        }
+    });
+
+    const createId = () => {
+        serialNumber++;
+        localStorage.setItem("serialNumber", serialNumber.toString());
+        return serialNumber;
+    }
+
+    function saveToLocalStorage(id, text, completed) {
+        savedTodos.push({id, text, completed});
+        localStorage.setItem("todos", JSON.stringify(savedTodos));
+        localStorage.setItem("serialNumber", serialNumber.toString());
+    }
+
+    function addTodoToDOM(id, text, completed) {
+        const $li = $("<li>").addClass("todo-item").attr("data-id", id).attr("data-text", text);
+        $li.html(`
+            <div class="todo-item__content">
+                <input type="checkbox" class="todo-checkbox" ${completed ? "checked" : ""}>
+                <span class="todo-text">${text}</span>
+                <div class="todo-buttons">
+                    <button class="todo-item__delete">Delete</button>
+                    <button class="btn btn-info btn-sm ms-2 show-details-btn" data-bs-toggle="modal" data-bs-target="#taskModal">Details</button>
+                </div>
+            </div>
+        `);
+        $(".js--todos-wrapper").append($li);
+
+        $li.find(".show-details-btn").on("click", function () {
+            const taskText = $li.find(".todo-text").text();
+            $("#taskText").text(taskText);
+        });
+
+        $li.find(".todo-item__delete").on("click", function () {
+            $li.remove();
+            saveTodos();
+        });
+    }
+
+    function saveTodos() {
+        savedTodos = [];
+        $(".todo-item").each(function () {
+            const id = $(this).data("id");
+            const text = $(this).data("text");
+            const completed = $(this).find(".todo-checkbox").prop("checked");
+            savedTodos.push({id, text, completed});
+        });
+        localStorage.setItem("todos", JSON.stringify(savedTodos));
+    }
+
+    initializeTodos();
 });
-
-
-const createId = () => {
-    serialNumber++;
-    localStorage.setItem("serialNumber", serialNumber.toString());
-    return serialNumber;
-}
-
-
-function saveToLocalStorage(id, text, completed) {
-    savedTodos.push({id, text, completed});
-    localStorage.setItem("todos", JSON.stringify(savedTodos));
-    localStorage.setItem("serialNumber", serialNumber.toString());
-}
-
-
-function addTodoToDOM(id, text, completed) {
-    const li = document.createElement("li");
-    li.classList.add("todo-item");
-    li.dataset.id = id;
-    li.dataset.text = text;
-    li.innerHTML = `
-        <div class="todo-item__content">
-            <input type="checkbox" class="todo-checkbox" ${completed ? "checked" : ""}>
-            <span class="todo-text">${text}</span>
-            <div class="todo-buttons">
-               <button class="todo-item__delete">Delete</button>
-               <button class="btn btn-info btn-sm ms-2 show-details-btn" data-bs-toggle="modal" data-bs-target="#taskModal">Details</button>
-             </div>
-        </div>
-        
-    `;
-    todosWrapper.appendChild(li);
-
-    li.querySelector(".show-details-btn").addEventListener("click", () => {
-        showTaskText(text);
-    });
-
-    li.querySelector(".todo-item__delete").addEventListener("click", () => {
-        deleteFromLocalStorage(id, li);
-    });
-
-    li.querySelector(".todo-checkbox").addEventListener("change", (e) => {
-        const completed = e.target.checked;
-        updateLocalStorage(id, completed);
-    });
-}
-
-
-function showTaskText(text) {
-    document.getElementById("taskText").innerText = text;
-}
-
-
-function updateLocalStorage(id, completed) {
-    const todoIndex = savedTodos.findIndex(todo => todo.id === id);
-    savedTodos[todoIndex].completed = completed;
-    localStorage.setItem("todos", JSON.stringify(savedTodos));
-}
-
-function deleteFromLocalStorage(id, li) {
-    todosWrapper.removeChild(li);
-    savedTodos = savedTodos.filter(todo => todo.id !== id);
-    localStorage.setItem("todos", JSON.stringify(savedTodos));
-}
-
-
-initializeTodos();
