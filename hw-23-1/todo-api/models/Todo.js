44d53df02ab3749337122b281a02f97d71280a2b
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-12-19 21:31:03 +0200

    Added script in html file, rewrote mongoURI

--- hw-23-1/todo-api/models/Todo.js
+++ hw-23-1/todo-api/models/Todo.js
@@ -1,9 +1,18 @@
-const mongoose = require('mongoose');
+const {Schema, model} = require('mongoose');
 
-const TodoSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    completed: { type: Boolean, default: false },
-    createdAt: { type: Date, default: Date.now }
-});
+const TodoSchema = new Schema({
+    text: {
+        type: String,
+        require: true
+    },
+    checked: {
+        type: Boolean,
+        require: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
 
-module.exports = mongoose.model('Todo', TodoSchema);
+module.exports = model('Todo', TodoSchema);
--- hw-23-1/todo-api/server.js
+++ hw-23-1/todo-api/server.js
@@ -10,9 +10,9 @@ app.use(express.json());
 app.use('/api', todoRoutes);
 
 
-const mongoURI = 'mongodb://localhost:27017/todo';
+const mongoURI = 'mongodb+srv://lebedlyuba:tVBABJA2Pf0qGqSX@cluster0.1qijc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 mongoose.connect(mongoURI)
-    .then(() => console.log('MongoDB connected'))
+    .then(() => console.log('MongoDB connected!'))
     .catch(err => console.error(err));
 
 
@@ -21,5 +21,6 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
