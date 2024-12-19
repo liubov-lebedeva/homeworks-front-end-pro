2e1a45ab890fb1c0b784f15ea26b365983d27cda
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-12-19 19:06:35 +0200

    Set up the express server

---
+++ hw-23-1/todo-api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+const mongoURI = 'mongodb://localhost:27017/todo';
+mongoose.connect(mongoURI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
