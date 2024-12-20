778211b32d862400f53c7c4bc78699921f15c807
liubov-lebedeva <lebed.lyuba@gmail.com>
2024-12-20 17:39:03 +0200

    Return json value

--- hw-23-1/api-for-todos/client/main.js
+++ hw-23-1/api-for-todos/client/main.js
@@ -1,6 +1,6 @@
 const getTodos = async () => {
     const response = await fetch("http://localhost:8080/api/todos");
-    console.log(response.body);
+    return await response.json();
 }
 
 getTodos();
